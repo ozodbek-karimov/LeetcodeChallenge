8073a4bec309c527a9b5355125374fdeacfba291
Ozodbek <ozodbekkarimov100@gmail.com>
2023-12-30 23:46:29 +0100

    new problem

---
+++ src/main/kotlin/LeetcodeSolutions/SearchInsertPosition.kt
@@ -0,0 +1,8 @@
+package LeetcodeSolutions
+
+/**
+*Creator: Karimov Ozodbek
+*Date 02/02/2023
+*/
+class SearchInsertPosition {
+}
---
+++ src/main/kotlin/learning_coroutines/Day1.kt
@@ -0,0 +1,8 @@
+package learning_coroutines
+
+/**
+*Creator: Karimov Ozodbek
+*Date 02/02/2023
+*/
+class Day1 {
+}
