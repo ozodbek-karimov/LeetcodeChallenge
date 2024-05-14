0a332ccaff7ed58dbe52acd11e56f5be50459325
ozodbek-karimov <ozodbekkarimov100@gmail.com>
2024-05-14 15:16:38 +0200

    LengthOfLastWord solved

---
+++ src/main/java/Day2/LengthOfLastWord.java
@@ -0,0 +1,29 @@
+package Day2;
+
+public class LengthOfLastWord {
+
+    public static void main(String[] args) {
+
+        System.out.println(lengthOfLastWord("Hello World"));
+        System.out.println(lengthOfLastWord("   fly me   to   the moon  "));
+        System.out.println(lengthOfLastWord("luffy is still joyboy"));
+    }
+
+
+    public static int lengthOfLastWord(String s) {
+
+        int length = 0;
+        int i = s.length() - 1;
+
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+}
