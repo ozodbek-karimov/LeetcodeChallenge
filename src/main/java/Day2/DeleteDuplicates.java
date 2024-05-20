b3a4424453d38ac9e746b37864948b5dfee2672e
ozodbek-karimov <ozodbekkarimov100@gmail.com>
2024-05-20 12:34:02 +0200

    LengthOfLastWord solved

---
+++ src/main/java/Day2/DeleteDuplicates.java
@@ -0,0 +1,38 @@
+package Day2;
+
+public class DeleteDuplicates {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+
+    }
+
+}
