dde0f735569e72ac726d30df50ff6fabbf8511a2
ozodbek-karimov <ozodbekkarimov100@gmail.com>
2024-05-24 15:20:03 +0200

    SingleNumber solved

---
+++ src/main/java/Day2/SingleNumber.java
@@ -0,0 +1,33 @@
+package Day2;
+
+import java.util.Arrays;
+
+public class SingleNumber {
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {2,2,1};
+        int[] arr2 = {4,1,2,1,2};
+        int[] arr3 = {1};
+
+        System.out.println(singleNumber(arr1));
+        System.out.println(singleNumber(arr2));
+        System.out.println(singleNumber(arr3));
+
+
+    }
+
+    public static int singleNumber(int[] numbs) {
+
+        int result = 0;
+
+        for (int num : numbs) {
+            result = result ^ num;
+        }
+
+        return result;
+
+
+    }
+
+}
