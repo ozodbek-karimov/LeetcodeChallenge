6a05af1e99bb364ff6109c3a51f9a50f96f110be
ozodbek-karimov <ozodbekkarimov100@gmail.com>
2024-05-29 02:43:00 +0200

    UglyNumber solved

---
+++ src/main/java/JavaChallenge/UglyNumber.java
@@ -0,0 +1,28 @@
+package JavaChallenge;
+
+public class UglyNumber {
+
+    public static void main(String[] args) {
+
+        System.out.println(isUgly(6));
+        System.out.println(isUgly(1));
+        System.out.println(isUgly(14));
+
+    }
+
+    public static boolean isUgly(int n) {
+
+        if(n <= 0) return false;
+        if(n == 1) return true;
+        if( n % 2 == 0){
+            return isUgly(n / 2);
+        }else if(n % 3 == 0){
+            return isUgly(n / 3);
+        }else if(n % 5 == 0){
+            return isUgly(n / 5);
+        }else{
+            return false;
+        }
+
+    }
+}
