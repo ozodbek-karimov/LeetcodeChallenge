620b91ade58e59f3f62d43ad07d3a29b4de91368
ozodbek-karimov <ozodbekkarimov100@gmail.com>
2024-06-02 02:26:54 +0200

    GuessNumber solved

---
+++ src/main/java/JavaChallenge/GuessNumber.java
@@ -0,0 +1,20 @@
+package JavaChallenge;
+
+public class GuessNumber {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int guessNumber(int n) {
+//        int start = 1, end = n, res = 0;
+//        while(start<=end){
+//            int mid=start+(end-start)/2;
+//            int gussRes=guess(mid);
+//            if(gussRes==0) return mid;
+//            else if(gussRes==-1) end=mid-1;
+//            else if(guess(gussRes)==1) start=mid+1;
+//        }
+//        return -1;
+    }
+}
