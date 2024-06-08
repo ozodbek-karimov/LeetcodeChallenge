301537141492f67d09e24d9548aee78c0dc5acc6
ozodbek-karimov <ozodbekkarimov100@gmail.com>
2024-06-08 12:44:09 +0200

    AppendCharacters solved

---
+++ src/main/java/JavaChallenge/AppendCharacters.java
@@ -0,0 +1,16 @@
+package JavaChallenge;
+
+public class AppendCharacters {
+
+    public int appendCharacters(String s, String t) {
+        int sIndex = 0, tIndex = 0, sLength = s.length(), tLength = t.length();
+        while (sIndex < sLength && tIndex < tLength){
+            if (s.charAt(sIndex) == t.charAt(tIndex)){
+                tIndex++;
+            }
+            sIndex++;
+        }
+        return tLength - tIndex;
+    }
+
+}
