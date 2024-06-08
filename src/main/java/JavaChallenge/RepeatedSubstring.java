baa53aca976473e48b8ee4ba91c15fa0e6f6fa41
ozodbek-karimov <ozodbekkarimov100@gmail.com>
2024-06-08 12:22:45 +0200

    ScoreOfSolution solved

--- src/main/java/JavaChallenge/RepeatedSubstring.java
+++ src/main/java/JavaChallenge/RepeatedSubstring.java
@@ -3,14 +3,14 @@ package JavaChallenge;
 public class RepeatedSubstring {
 
     public boolean repeatedSubstringPattern(String str) {
-        int l = str.length();
-        for(int i=l/2;i>=1;i--) {
-            if(l%i==0) {
-                int m = l/i;
-                String subS = str.substring(0,i);
-                StringBuilder sb = new StringBuilder();
-                sb.append(subS.repeat(m));
-                if(sb.toString().equals(str)) return true;
+       int length = str.length();
+        for (int i = length / 2; i >= 1; i--) {
+            if (length % i == 0){
+                int middle =  length / i;
+                String substring = str.substring(0, i);
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append(substring.repeat(middle));
+                if (stringBuilder.toString().equals(str)) return true;
             }
         }
         return false;
---
+++ src/main/java/JavaChallenge/ScoreOfSolution.java
@@ -0,0 +1,13 @@
+package JavaChallenge;
+
+public class ScoreOfSolution {
+
+
+    public int scoreOfString(String s) {
+        int sum = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            sum += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        return sum;
+    }
+}
