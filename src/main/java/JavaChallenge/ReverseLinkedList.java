8e9bfb33bf36ada65b6b416f40ef7d4569ea10c7
ozodbek-karimov <ozodbekkarimov100@gmail.com>
2024-06-12 23:06:20 +0200

    ReverseLinkedList solved

---
+++ src/main/java/JavaChallenge/ReverseLinkedList.java
@@ -0,0 +1,33 @@
+package JavaChallenge;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class ReverseLinkedList {
+    public ListNode reverseList(ListNode currentNode) {
+        ListNode previousNode = null;
+
+        while (currentNode != null) {
+            ListNode nextNode = currentNode.next;
+            currentNode.next = previousNode;
+            previousNode = currentNode;
+            currentNode = nextNode;
+        }
+
+        return previousNode;
+    }
+}
